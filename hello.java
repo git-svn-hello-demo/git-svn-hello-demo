6d676ab20b6b5027ad7953b6e09dfecebd458bcf
imprity <imprity041@gmail.com>
2026-02-07 16:10:40 +0900

    charlie/goodbye

--- hello.java
+++ hello.java
@@ -7,4 +7,8 @@ class Hello {
         System.out.println("hello");
         System.out.println("world");
     }
+
+    public static void sayGoodbye() {
+        System.out.println("goodbye");
+    }
 }
